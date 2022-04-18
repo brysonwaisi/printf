38b5d29b1f48d781b610242bd72f576341efeefc
flovian <flovian.owiti@gmail.com>
2022-04-18 21:14:01 +0000

    Quick update

--- _printf.c
+++ _printf.c
@@ -20,11 +20,7 @@ void cleanup(va_list args, buffer_t *output)
  * run_printf - Reads through the format string for _printf.
  * @format: Character string to print - may contain directives.
  * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
-<<<<<<< HEAD
-=======
- * int: function unsigned.
->>>>>>> d3ba0832eceb2c4ec1650de84e7b0ac23ab3de0b
+ * @args: A va_list of arguments
  * Return: The number of characters stored to output.
  */
 int run_printf(const char *format,  va_list args, buffer_t *output)
--- handlers.c
+++ handlers.c
@@ -4,8 +4,6 @@ unsigned char handle_flags(const char *flag, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
 
 /**
  * handle_flags - Matches flags with corresponding values.
@@ -160,15 +158,15 @@ int handle_precision(va_list args, const char *modifier, char *index)
 /**
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
- * @specifier: A pointer to a potential conversion specifier.
- *
+ * @int: integer
+ * @char: Character
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
  */
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char)
 {
-	int i;
+	int i = 0;
 	converter_t converters[] = {
 		{'c', convert_c},
 		{'s', convert_s},
